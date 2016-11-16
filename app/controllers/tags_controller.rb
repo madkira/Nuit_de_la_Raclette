fc6153e86ded62850b9b30564fef70cb3832a03d
Raphaël KUMAR <enlil.baal@gmail.com>
2016-11-16 20:38:16 +0100

    add controller and html view for tags

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,10 @@
+class TagsController < ApplicationController
+
+  def index
+    @tags = Tag.order(name: :asc)
+  end
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'home#index'
-
+  get 'tags' =>'tags#index'
+  get 'tag/:id' =>'tags#show'
   scope "(:locale)" do
-  	
+
     get '/:locale' => 'home#index'
   end
 
